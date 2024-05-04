f0c28a2365073564496a5fd84d02b7af3cc2ccb3
Simon Woodward <simon@redcabbage.org>
2024-05-04 16:36:02 +0200

    Make pretty

--- progressbar/progressbar/__init__.py
+++ progressbar/progressbar/__init__.py
@@ -3,10 +3,10 @@ import time
 import tqdm
 
 def dostuff():
-    bars = [tqdm.tqdm(total=100, desc=f"Bar {i}", position=i+5)for i in range(5)]
+    bars = [tqdm.tqdm(total=100, desc=f"Bar {i}", position=i+5, colour='green') for i in range(5)]
     while bars:
         bar = bars[random.randint(0, len(bars) - 1)]
-        bar.update()
+        bar.update(2)
         if bar.n == 100:
             bars.remove(bar)
 
